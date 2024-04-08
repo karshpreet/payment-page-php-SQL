a1357349d9dd6b3df5638505ed245a4562d0dab9
karshpreet <121468827+karshpreet@users.noreply.github.com>
2024-04-08 22:25:47 +0530

    Create config.php
    This is config.php file.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+//host
+define("HOST", "localhost");
+
+//dbname
+define("DBNAME", "pay");
+
+//user
+define("USER", "root");
+
+//password
+define("PASS", "");
+
+
+$conn = new PDO("mysql:host=".HOST."; dbname=".DBNAME.";", USER, PASS);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//if ($conn == true) {
+
+  //  echo "Connection is a success";
+//}
+//else {
+//    "error";
+//}
